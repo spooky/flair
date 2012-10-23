032cef88a74e43a4183437b2029e9a93bea41f21
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2012-10-23 22:54:50 +0200

    removed CoffeeSharp - caused the app to start for a long time. Using SassAndCoffee compiler instead

--- Flair/Transformations/CoffeeToJs.cs
+++ Flair/Transformations/CoffeeToJs.cs
@@ -1,10 +1,13 @@
-﻿using CoffeeSharp;
+﻿using SassAndCoffee.Core;
+using SassAndCoffee.JavaScript;
+using SassAndCoffee.JavaScript.CoffeeScript;
 
 namespace Flair.Transformations
 {
     public class CoffeeToJs : ITransform
     {
-        private readonly CoffeeScriptEngine engine;
+        private readonly IInstanceProvider<IJavaScriptRuntime> runtimeProvider;
+        private readonly IInstanceProvider<IJavaScriptCompiler> compilerProvider;
 
         public string DisplayText
         {
@@ -13,12 +16,25 @@ namespace Flair.Transformations
 
         public CoffeeToJs()
         {
-            engine = new CoffeeScriptEngine();
+            runtimeProvider = new InstanceProvider<IJavaScriptRuntime>(() => (IJavaScriptRuntime)new IEJavaScriptRuntime());
+            compilerProvider = CreateCompilerProvider(runtimeProvider);
         }
 
         public string Transform(string source)
         {
-            return engine.Compile(source);
+            // bare = true skips the function safety wrapper
+            const bool bare = false;
+            var args = new[] { (object)(bare ? 1 : 0) };
+
+            using (var compiler = compilerProvider.GetInstance())
+            {
+                return compiler.Compile(source, args);
+            }
+        }
+
+        private static IInstanceProvider<IJavaScriptCompiler> CreateCompilerProvider(IInstanceProvider<IJavaScriptRuntime> jsRuntimeProvider)
+        {
+            return new InstanceProvider<IJavaScriptCompiler>(() => (IJavaScriptCompiler)new CoffeeScriptCompiler(jsRuntimeProvider));
         }
     }
 }
